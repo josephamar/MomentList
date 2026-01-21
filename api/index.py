e116648832e9daa34b36bb71d0c3ea3b10fcfb01
Arié Amar <arieamar26@gmail.com>
2026-01-21 17:22:35 +0100

    Simple Flask test

--- api/index.py
+++ api/index.py
@@ -1,13 +1,7 @@
-import sys
-import os
-import traceback
+from flask import Flask
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+app = Flask(__name__)
 
-try:
-    from flask_app.app import create_app
-    app = create_app()
-except Exception as e:
-    print("BOOT ERROR:", repr(e))
-    traceback.print_exc()
-    raise
+@app.get("/")
+def home():
+    return "OK ✅ Flask is running on Vercel"
